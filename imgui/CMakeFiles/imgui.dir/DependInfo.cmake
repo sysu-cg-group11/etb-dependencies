a6a46cc2c8cc300c3e4a951be38dea4a2e50a982
Miguel Chan <vvchan@outlook.com>
2019-06-01 13:55:59 +0800

    init

---
+++ imgui/CMakeFiles/imgui.dir/DependInfo.cmake
@@ -0,0 +1,37 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/mig/Desktop/junior-spring/graphics/Enjoy-The-Beach/dependencies/imgui/src/imgui.cpp" "/Users/mig/Desktop/junior-spring/graphics/Enjoy-The-Beach/dependencies/imgui/CMakeFiles/imgui.dir/src/imgui.cpp.o"
+  "/Users/mig/Desktop/junior-spring/graphics/Enjoy-The-Beach/dependencies/imgui/src/imgui_demo.cpp" "/Users/mig/Desktop/junior-spring/graphics/Enjoy-The-Beach/dependencies/imgui/CMakeFiles/imgui.dir/src/imgui_demo.cpp.o"
+  "/Users/mig/Desktop/junior-spring/graphics/Enjoy-The-Beach/dependencies/imgui/src/imgui_draw.cpp" "/Users/mig/Desktop/junior-spring/graphics/Enjoy-The-Beach/dependencies/imgui/CMakeFiles/imgui.dir/src/imgui_draw.cpp.o"
+  "/Users/mig/Desktop/junior-spring/graphics/Enjoy-The-Beach/dependencies/imgui/src/imgui_impl_glfw.cpp" "/Users/mig/Desktop/junior-spring/graphics/Enjoy-The-Beach/dependencies/imgui/CMakeFiles/imgui.dir/src/imgui_impl_glfw.cpp.o"
+  "/Users/mig/Desktop/junior-spring/graphics/Enjoy-The-Beach/dependencies/imgui/src/imgui_impl_opengl3.cpp" "/Users/mig/Desktop/junior-spring/graphics/Enjoy-The-Beach/dependencies/imgui/CMakeFiles/imgui.dir/src/imgui_impl_opengl3.cpp.o"
+  "/Users/mig/Desktop/junior-spring/graphics/Enjoy-The-Beach/dependencies/imgui/src/imgui_stdlib.cpp" "/Users/mig/Desktop/junior-spring/graphics/Enjoy-The-Beach/dependencies/imgui/CMakeFiles/imgui.dir/src/imgui_stdlib.cpp.o"
+  "/Users/mig/Desktop/junior-spring/graphics/Enjoy-The-Beach/dependencies/imgui/src/imgui_widgets.cpp" "/Users/mig/Desktop/junior-spring/graphics/Enjoy-The-Beach/dependencies/imgui/CMakeFiles/imgui.dir/src/imgui_widgets.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "GLFW_DLL"
+  "IMGUI_IMPL_OPENGL_LOADER_GLAD"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "dependencies/imgui/include/imgui"
+  "dependencies/glad/include"
+  "dependencies/GLFW/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/Users/mig/Desktop/junior-spring/graphics/Enjoy-The-Beach/dependencies/glad/CMakeFiles/glad.dir/DependInfo.cmake"
+  "/Users/mig/Desktop/junior-spring/graphics/Enjoy-The-Beach/dependencies/GLFW/src/CMakeFiles/glfw.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
